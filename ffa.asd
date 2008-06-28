465461ac935650e82100cabdc77c02f51eeffc8c
Tamas K Papp <tpapp@princeton.edu>
2008-06-28 10:52:38 +0200

    Reorganization of non-FFI functions into array-operations package.

--- ffa.asd
+++ ffa.asd
@@ -8,9 +8,6 @@
   :author "Tamas K Papp"
   :license "GPL"
   :components ((:file "package")
-	       (:file "types" :depends-on ("package"))
-	       (:file "ffa" :depends-on ("types"))
-	       (:file "operations" :depends-on ("ffa"))
-	       (:file "pointer" :depends-on ("ffa")))
-  :depends-on (:cffi :cl-utilities :metabang-bind :iterate))
+	       (:file "pointer" :depends-on ("package")))
+  :depends-on (:cffi :cl-utilities :metabang-bind :iterate :array-operations))
 
--- package.lisp
+++ package.lisp
@@ -1,22 +1,7 @@
 (defpackage :ffa
-  (:use :common-lisp :cl-utilities :bind :iterate)
+  (:use :common-lisp :cl-utilities :bind :iterate :array-operations)
   (:shadowing-import-from :iterate :collecting :collect)
   (:export 
-
-   ;; ffa
-
-   make-ffa displace-array flatten-array find-original-array
-   find-or-displace-to-flat-array array-copy array-map array-map! 
-   array-convert
-
-   ;; operations
-
-   array-reduce array-max array-min array-sum array-product
-   array-count array-range array-mean dot-product outer-product
-   array-elementwise-operation array+ array- array* array/
-   array-scalar+ array-scalar- array-scalar* array-scalar/
-   array-reciprocal array-negate array-map-list array-map-values
-   index-extrema array-extrema
 
    ;; pointer
 
